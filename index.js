3a9a0b217e6f62bf28f81a6b3929ec5ed4a7052a
Deepak Kadarivel <deepak@Deepaks-MacBook-Pro.local>
2018-09-20 11:33:07 +0530

    node mail service commit

--- index.js
+++ index.js
@@ -1,80 +1,88 @@
-const express = require("express");
+var express = require("express");
 var bodyParser = require("body-parser");
-var nodemailer = require('nodemailer');
+var nodemailer = require("nodemailer");
 
-const app = express();
+var app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 
-app.use((request, response, next) => {
-  console.log(request.headers);
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
-app.use((request, response, next) => {
-  request.chance = Math.random();
-  next();
-});
-
-app.get("/", (request, response) => {
-  response.json({
-    chance: request.chance
-  });
-});
-
-app.post("/mail", (req, res) => {
-  var firstName = req.body.firstName;
-  var lastName = req.body.lastName;
-  var email = req.body.email;
-  var role = req.body.role;
-  var usage = req.body.usage;
-  var contactDetail = req.body.contactDetail;
-  var dateOption1 = req.body.dateOption1;
-  var dateOption2 = req.body.dateOption2;
-  var how = "";
-  
-  const msg = {
-      html: "<strong>First name</strong>" + firstName + "</br>" +
-      "<strong>Last name: </strong>" + lastName + "</br>" +
-      "<strong>Email: </strong>" + email + "</br>" +
-      "<strong>Role: </strong>" + role + "</br>" +
-      "<strong>Usage: </strong>" + usage + "</br>" +
-      "<strong>Contact detail: </strong>" + contactDetail + "</br>" +
-      "<strong>date option #1</strong>" + dateOption1 + "</br>" +
-      "<strong>date option #2</strong>" + dateOption2 + "</br>" +
-      "<strong>dHow do you know us: </strong>" + how + "</br>" 
-    };
-    sendMail(msg);
-    res.end("yes");
+app.post("/", function(request, response) {
+  console.log(request.body); // your JSON
+  sendMail(request.body);
+  response.send(request.body); // echo the result back
 });
 
 function sendMail(data) {
-    console.log('Starting mail server');
-    console.log(data.html);
-    var transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-          user: 'zinclabtest@gmail.com',
-          pass: 'Y0uKn0wIt'
-        }
-      });
-      
-      var mailOptions = {
-        from: 'zinclabtest@gmail.com',
-        to: 'zinclabtest@gmail.com',
-        subject: 'SSignup demo request.',
-        text: 'A new user has requested for a signup demo.',
-        html: data.html
-      };
-      
-      transporter.sendMail(mailOptions, function(error, info){
-        if (error) {
-          console.log(error);
-        } else {
-          console.log('Email sent: ' + info.response);
-        }
-      });
+  var transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "zinclabtest@gmail.com",
+      pass: "Y0uKn0wIt"
+    }
+  });
+
+  var mailOptions = {
+    from: "zinclabtest@gmail.com",
+    to: "PARTNERSHIPS@ZINCLEARNINGLABS.COM",
+    subject: "Signup demo request.",
+    text: "A new user has requested for a signup demo.",
+    html:
+      "<p><strong>First name: </strong>" +
+      data.firstName +
+      "</p>" +
+      "<p><strong>Last name: </strong>" +
+      data.lastName +
+      "</p>" +
+      "<p><strong>Email: </strong>" +
+      data.email +
+      "</p>" +
+      "<p><strong>Role: </strong>" +
+      data.role +
+      "</p>" +
+      "<p><strong>Usage: </strong>" +
+      data.usage +
+      "</p>" +
+      "<p><strong>Contact detail: </strong>" +
+      data.contactDetail +
+      "</p>" +
+      "<p><strong>date option #1: </strong>" +
+      data.dateOption1 +
+      "</p>" +
+      "<p><strong>time option #1: </strong>" +
+      data.timeOption1 +
+      "</p>" +
+      "<p><strong>date option #2: </strong>" +
+      data.dateOption2 +
+      "</p>" +
+      "<p><strong>time option #2: </strong>" +
+      data.timeOption2 +
+      "</p>" +
+      "<p><strong>How do you know us: </strong>" +
+      data.how +
+      "</p>"
+  };
+
+  transporter.sendMail(mailOptions, function(error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
+    }
+  });
 }
 
-app.listen(3033);
+app.listen(3000);
